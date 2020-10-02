0818e69a30d31cd7fd4a35cd17181cbd2b758712
codeItSidz <sidz23999@gmail.com>
2020-10-02 16:44:02 +0530

    Initial Commit

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/../public'));
+
+
+
+app.listen(port, () => {
+    console.log(`Server Listening on Port ${port}`);
+});
